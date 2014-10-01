c604a38eb837cc8c6e0d5e7fd563219031b6b1c1
Megsammor <megansmorrison@gmail.com>
2014-09-30 21:07:11 -0400

    Baby steps
    Working at it so please excuse baby steps. Definitely ultimately want
    things white and simple and clean. Website will be called Studio Belge.
    Wondering if you can do a vertical overflow: hidden and trying to
    remember how to create my nag bar as blocks underneath that will
    automatically space out.

--- week_5_advanced_js_jquery/my_cat_blog/js/index.js
+++ week_5_advanced_js_jquery/my_cat_blog/js/index.js
@@ -9,11 +9,40 @@ var posts = [
 		snippet: 'Wow...thats all I can say. I was surfing /cats on reddit and came across the cutest cat pictures yet! Take a look!'
 	}, {
 		image: 'http://placepu.gs/200/200',
-		title: 'Wierd cats!',
+		title: 'Weird cats!',
 		snippet: 'I saw these weird looking cats on the interwebs today. If you can tell me what breed they are, please do so in the comments!'
 	}, {
 		image: 'http://placekitten.com/g/200/200',
-		title: 'Cutness overload!',
+		title: 'Cuteness overload!',
 		snippet: 'OMG people, are you ready for some heavy duty cuteness? I know I am. Check out these cats! I can\'t even handle this. Too cute!'
 	}
 ];
+
+var html = '';
+
+function generatePost(posts) {
+	var fragment = '';
+
+	fragment += '<div class="post clearfix">';
+	fragment += '<img class="post-image float-left" src="' + posts.image + '">';
+	fragment += '<div class="post-content float-left">';
+	fragment += '<h3 class="post-title">' + posts.title + '</h3>';
+	fragment += '<p>';
+	fragment += posts.snippet;
+	fragment += '</p>';
+	fragment += '</div>';
+	fragment += '</div>';
+	return fragment;
+}
+
+for (var i = 0; i < posts.length; i++) {
+	html += generatePost(posts[i]);
+
+}
+
+
+//below is the javascript that is replaced by the jquery function below
+//document.getElementById('posts').innerHTML = html;
+
+$('#posts').html(html);
+
---
+++ week_7_integration/photographer_page/js/index.js
@@ -0,0 +1,6 @@
+$function () {
+
+});
+$(document).ready(function() {
+	var background = $('.lightbox-background')	
+});
